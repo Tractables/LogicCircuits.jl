8c9cced2fae4e43c2333774f0a141956be5c55e5
Pasha Khosravi <khosravipasha@gmail.com>
2019-07-16 16:08:02 -0700

    Test for reading prob_circuit correctly from file

--- test/CircuitParserTest.jl
+++ test/CircuitParserTest.jl
@@ -30,3 +30,27 @@ end
    @test lc isa LogicalCircuit△
 
 end
+
+@testset "Load Small PSDD and test methods" begin
+   file = "test/circuits/little_4var.psdd"
+   prob_circuit = load_psdd_prob_circuit(file);
+   @test prob_circuit isa Vector{<:ProbCircuitNode};
+
+   # Testing number of nodes and parameters
+   @test  9 == num_parameters(prob_circuit)
+   @test 20 == size(prob_circuit)[1]
+   
+   # Testing Read Parameters
+   EPS = 1e-7
+   @test abs(prob_circuit[13].log_thetas[1] - (-1.6094379124341003)) < EPS
+   @test abs(prob_circuit[13].log_thetas[2] - (-1.2039728043259361)) < EPS
+   @test abs(prob_circuit[13].log_thetas[3] - (-0.916290731874155)) < EPS
+   @test abs(prob_circuit[13].log_thetas[4] - (-2.3025850929940455)) < EPS
+
+   @test abs(prob_circuit[18].log_thetas[1] - (-2.3025850929940455)) < EPS
+   @test abs(prob_circuit[18].log_thetas[2] - (-2.3025850929940455)) < EPS
+   @test abs(prob_circuit[18].log_thetas[3] - (-2.3025850929940455)) < EPS
+   @test abs(prob_circuit[18].log_thetas[4] - (-0.35667494393873245)) < EPS
+
+   @test abs(prob_circuit[20].log_thetas[1] - (0.0)) < EPS
+end
