640aaea94c5f8223b9f565c4d4a83a531eb2958b
Pasha Khosravi <khosravipasha@gmail.com>
2019-07-17 18:35:09 -0700

    Vtree skeleton

---
+++ src/Juice/Vtree.jl
@@ -0,0 +1,35 @@
+#############
+# Vtree
+#############
+
+"Root of the vtree node hiearchy"
+abstract type VtreeNode end
+
+struct VtreeLeafNode <: VtreeNode
+    index::Int
+    var::Var
+end
+
+struct VtreeInnerNode <: VtreeNode
+    index::Int
+    left::VtreeNode
+    right::VtreeNode
+    var_count::Int
+    variables::Set{VtreeNode}
+end
+
+#####################
+# Constructor
+#####################
+
+IsLeaf(n::VtreeLeafNode) = true
+IsLeaf(n::VtreeInnerNode) = false
+
+Variables(n::VtreeLeafNode) = Set([n.var])
+Variables(n::VtreeInnerNode) = n.variables
+
+VariableCount(n::VtreeLeafNode) = 1
+VariableCount(n::VtreeInnerNode) = n.var_count
+
+
+
