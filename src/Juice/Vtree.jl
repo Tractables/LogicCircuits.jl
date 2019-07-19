39b5de9513c5f29a5dd81cdf26438007783c2db8
Pasha Khosravi <khosravipasha@gmail.com>
2019-07-18 17:43:28 -0700

    Vtree Save with tests

--- src/Juice/Vtree.jl
+++ src/Juice/Vtree.jl
@@ -31,5 +31,62 @@ Variables(n::VtreeInnerNode) = n.variables
 VariableCount(n::VtreeLeafNode) = 1
 VariableCount(n::VtreeInnerNode) = n.var_count
 
+function OrderNodesLeavesBeforeParents(root::VtreeNode)::Vector{VtreeNode}
+    # Running BFS
+    visited = Vector{VtreeNode}()
+    visited_idx = 0
+    push!(visited, root)
+
+    while visited_idx < length(visited)
+        visited_idx += 1
+        
+        if visited[visited_idx] isa VtreeLeafNode
+            
+        else
+            left = visited[visited_idx].left
+            right = visited[visited_idx].right
+            
+            push!(visited, right)
+            push!(visited, left)
+        end
+    end
+
+    reverse(visited)
+end
 
 
+const VTREE_FORMAT = """c ids of vtree nodes start at 0
+c ids of variables start at 1
+c vtree nodes appear bottom-up, children before parents
+c
+c file syntax:
+c vtree number-of-nodes-in-vtree
+c L id-of-leaf-vtree-node id-of-variable
+c I id-of-internal-vtree-node id-of-left-child id-of-right-child
+c
+"""
+
+function save(vtree::Vector{VtreeNode}, file::AbstractString)
+    open(file, "w") do f
+        order = OrderNodesLeavesBeforeParents(vtree[end]);
+        vtree_count = length(vtree)
+
+        write(f, VTREE_FORMAT)
+        
+        write(f, "vtree $vtree_count\n")
+        for (ind, node) in enumerate(order)
+            if node isa VtreeLeafNode
+                node_index = node.index
+                node_variable = node.var
+                write(f, "L $node_index $node_variable\n")
+            elseif node isa VtreeInnerNode
+                node_index = node.index
+                left = node.left.index
+                right = node.right.index
+                write(f, "I $node_index $left $right\n")
+            else
+                throw("Invalid Vtree Node Type")
+            end
+        end
+    end
+end
--- test/VtreeParserTest.jl
+++ test/VtreeParserTest.jl
@@ -17,27 +17,41 @@ using Test;
 
     vtree = compile_vtree_format_lines(vtree_lines)
 
-    for i = 1:4
-        @test vtree[i] isa VtreeLeafNode
+    function test_vtree(vtree)
+        for i = 1:4
+            @test vtree[i] isa VtreeLeafNode
+        end
+        for i = 5:7
+            @test vtree[i] isa VtreeInnerNode
+        end
+    
+        @test Variables(vtree[1]) == Set([1])
+        @test Variables(vtree[2]) == Set([2])
+        @test Variables(vtree[3]) == Set([3])
+        @test Variables(vtree[4]) == Set([4])
+    
+        @test Variables(vtree[5]) == Set([1,2])
+        @test Variables(vtree[6]) == Set([3,4])
+        @test Variables(vtree[7]) == Set([1,2,3,4])
+    
+        for i = 1:4
+            @test VariableCount(vtree[i]) == 1
+        end
+        @test VariableCount(vtree[5]) == 2
+        @test VariableCount(vtree[6]) == 2
+        @test VariableCount(vtree[7]) == 4
     end
-    for i = 5:7
-        @test vtree[i] isa VtreeInnerNode
-    end
 
-    @test Variables(vtree[1]) == Set([1])
-    @test Variables(vtree[2]) == Set([2])
-    @test Variables(vtree[3]) == Set([3])
-    @test Variables(vtree[4]) == Set([4])
+    test_vtree(vtree)
 
-    @test Variables(vtree[5]) == Set([1,2])
-    @test Variables(vtree[6]) == Set([3,4])
-    @test Variables(vtree[7]) == Set([1,2,3,4])
+     # Now testing save
+     #   Save the vtree, load it from file, and then run the same tests
+    temp_path = "test/circuits/little_4var_temp.vtree"
+    save(vtree, temp_path)
 
-    for i = 1:4
-        @test VariableCount(vtree[i]) == 1
-    end
-    @test VariableCount(vtree[5]) == 2
-    @test VariableCount(vtree[6]) == 2
-    @test VariableCount(vtree[7]) == 4
+    vtree2 = compile_vtree_format_lines(parse_vtree_file(temp_path))
+    test_vtree(vtree2)
 
+    rm(temp_path)
 end
+
