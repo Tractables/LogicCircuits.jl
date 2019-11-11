4d15a418ddb1e6961280c317373ec2c33ee4e4da
Guy Van den Broeck <guyvdb@cs.ucla.edu>
2019-11-10 22:49:05 -0300

    make vtree a type argument

--- src/Juice/Logical/StructuredLogicalCircuits.jl
+++ src/Juice/Logical/StructuredLogicalCircuits.jl
@@ -4,42 +4,42 @@
 #####################
 
 "Root of the structure logical circuit node hierarchy"
-abstract type StructLogicalΔNode <: LogicalΔNode end
+abstract type StructLogicalΔNode{V<:VtreeNode} <: LogicalΔNode end
 
 "A structured logical leaf node"
-abstract type StructLogicalLeafNode <: StructLogicalΔNode end
+abstract type StructLogicalLeafNode{V} <: StructLogicalΔNode{V} end
 
 "A structured logical inner node"
-abstract type StructLogicalInnerNode <: StructLogicalΔNode end
+abstract type StructLogicalInnerNode{V} <: StructLogicalΔNode{V} end
 
 "A structured logical literal leaf node, representing the positive or negative literal of its variable"
-struct StructLiteralNode <: StructLogicalLeafNode
+struct StructLiteralNode{V} <: StructLogicalLeafNode{V}
     literal::Lit
-    vtree::VtreeLeafNode
+    vtree::V
 end
 
 """
 A structured logical constant leaf node, representing true or false.
 These are the only structured nodes that don't have an associated vtree node (cf. SDD file format)
 """
-abstract type StructConstantNode <: StructLogicalInnerNode end
-struct StructTrueNode <: StructConstantNode end
-struct StructFalseNode <: StructConstantNode end
+abstract type StructConstantNode{V} <: StructLogicalInnerNode{V} end
+struct StructTrueNode{V} <: StructConstantNode{V} end
+struct StructFalseNode{V} <: StructConstantNode{V} end
 
 "A structured logical conjunction node"
-struct Struct⋀Node <: StructLogicalInnerNode
-    children::Vector{StructLogicalΔNode}
-    vtree::VtreeInnerNode
+struct Struct⋀Node{V} <: StructLogicalInnerNode{V}
+    children::Vector{<:StructLogicalΔNode{<:V}}
+    vtree::V
 end
 
 "A structured logical disjunction node"
-struct Struct⋁Node <: StructLogicalInnerNode
-    children::Vector{StructLogicalΔNode}
-    vtree::VtreeNode # could be leaf or inner
+struct Struct⋁Node{V} <: StructLogicalInnerNode{V}
+    children::Vector{<:StructLogicalΔNode{<:V}}
+    vtree::V # could be leaf or inner
 end
 
 "A structured logical circuit represented as a bottom-up linear order of nodes"
-const StructLogicalCircuit = AbstractVector{<:StructLogicalΔNode}
+const StructLogicalCircuit{V} = AbstractVector{<:StructLogicalΔNode{V}}
 
 #####################
 # traits
