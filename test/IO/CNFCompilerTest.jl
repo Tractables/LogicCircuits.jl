167322160065dd4fcd95c4f7e5b01e8083360143
Guy Van den Broeck <guyvdb@gmail.com>
2019-12-08 10:43:10 -0800

    add sdd sizes to test

--- test/IO/CNFCompilerTest.jl
+++ test/IO/CNFCompilerTest.jl
@@ -7,13 +7,13 @@ include("../helper/ValidateSdd.jl")
 
 @testset "CNF file parser tests" begin
 
-   cnfs = [ ("easy","C17_mince",32),
-            ("easy","majority_mince",32),
-            ("easy","b1_mince",8),
-            ("easy","cm152a_mince",2048),
-            ("iscas89","s208.1.scan",262144)]
+   cnfs = [ ("easy","C17_mince",32,92,45),
+            ("easy","majority_mince",32,132,61),
+            ("easy","b1_mince",8,169,84),
+            ("easy","cm152a_mince",2048,127,62),
+            ("iscas89","s208.1.scan",262144,1942,927)]
       
-   for (suite, name, count) in cnfs
+   for (suite, name, count, size, nodes) in cnfs
 
       cnf = load_cnf("test/cnfs/$suite/$name.cnf")
       vtree = load_vtree("test/cnfs/$suite/$name.min.vtree");
@@ -23,11 +23,11 @@ include("../helper/ValidateSdd.jl")
       cnfΔ = node2dag(compile_cnf(mgr, cnf), TrimSdd)
 
       # println("Final number of edges: ", num_edges(cnfΔ))
-      # println("Final SDD size: ", sdd_size(cnfΔ))
-      # println("Final SDD node count: ", sdd_num_nodes(cnfΔ))
       # println("Final SDD model count: ", model_count(cnfΔ))
 
       @test model_count(cnfΔ) == count
+      @test sdd_size(cnfΔ) == size
+      @test sdd_num_nodes(cnfΔ) == nodes
 
       validate(cnfΔ::Dag)
 
