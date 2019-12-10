63ea7140b597236848d11f630c58f0b6ffc37440
Guy Van den Broeck <guyvdb@gmail.com>
2019-12-09 19:36:31 -0800

    print node stats for debugging

--- test/IO/CNFCompilerTest.jl
+++ test/IO/CNFCompilerTest.jl
@@ -7,11 +7,13 @@ include("../helper/ValidateSdd.jl")
 
 @testset "CNF file parser tests" begin
 
-   cnfs = [ ("easy","C17_mince",32,92,45),
-            ("easy","majority_mince",32,132,61),
-            ("easy","b1_mince",8,169,84),
-            ("easy","cm152a_mince",2048,127,62),
-            ("iscas89","s208.1.scan",262144,1942,927)]
+   cnfs = [ 
+            ("easy","C17_mince",32,92,45)
+            # ("easy","majority_mince",32,132,61)
+            # ("easy","b1_mince",8,169,84)
+            # ("easy","cm152a_mince",2048,127,62)
+            # ("iscas89","s208.1.scan",262144,1942,927)
+          ]
       
    for (suite, name, count, size, nodes) in cnfs
 
@@ -22,8 +24,9 @@ include("../helper/ValidateSdd.jl")
       # cnfΔ = @time compile_cnf(mgr, cnf)
       cnfΔ = node2dag(compile_cnf(mgr, cnf), TrimSdd)
 
-      # println("Final number of edges: ", num_edges(cnfΔ))
-      # println("Final SDD model count: ", model_count(cnfΔ))
+      println("Sdd size: ", sdd_size(cnfΔ))
+
+      println(node_stats(cnfΔ))
 
       @test model_count(cnfΔ) == count
       @test sdd_size(cnfΔ) == size
