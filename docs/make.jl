7715fc222138d99005fddc3777992e12e7706175
Pasha Khosravi <khosravipasha@gmail.com>
2020-03-05 20:51:04 -0800

    documenter auto deploy

--- docs/make.jl
+++ docs/make.jl
@@ -13,6 +13,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/Juice-jl/LogicCircuits.jl.git",
+)
