80758507541e0684cc3cd350ff1ed6a57f73c9ff
Shreyas <shreyaskowshik@gmail.com>
2020-07-24 08:51:05 +0530

    Add clone and merge candidates

--- src/transformations.jl
+++ src/transformations.jl
@@ -1,5 +1,6 @@
 export smooth, forget, propagate_constants, deepcopy, condition, replace_node, 
-    split, clone, merge, split_candidates, random_split, split_step, struct_learn
+    split, clone, merge, split_candidates, random_split, split_step, struct_learn,
+    clone_candidates, merge_candidates
 
 """
 Create an equivalent smooth circuit from the given circuit.
@@ -253,6 +254,68 @@ function split_candidates(circuit::Node)::Tuple{Vector{Tuple{Node, Node}}, Dict{
     candidates, scope
 end
 
+function clone_candidates(circuit::Node)::Dict{Node, Vector{Node}}
+    candidates = Dict{Node, Vector{Node}}()
+    parents = Dict{Node, Vector{Node}}()
+
+    f_con(n) = begin
+        false
+    end
+    f_lit(n) = begin
+        false
+    end
+    f_a(n, cv) = begin
+        for c in children(n)
+            if !(GateType(c) isa ⋁Gate)
+                continue
+            end
+
+            if c in keys(parents)
+                push!(parents[c], n)
+            else
+                parents[c] = [n]
+            end
+        end
+        false
+    end
+    f_o(n, cv) = begin
+        if !(n in keys(parents))
+            parents[n] = []
+        end
+        true
+    end
+
+    foldup_aggregate(circuit, f_con, f_lit, f_a, f_o, Bool)
+
+    # Find candidates
+    candidates = filter(p->(length(last(p)) == 2), parents) # Set of AND gates shared by exactly 2 OR gates
+    candidates
+end
+
+function merge_candidates(circuit::Node)::Vector{Vector{Node}}
+    signatures = prob_equiv_signature(circuit, k)
+    decision_nodes_by_signature = groupby(n -> signatures[n], ⋁_nodes(circuit))
+
+    candidates = []
+    for (prob_base, nodes) in decision_nodes_by_signature
+        if prob_base == ones(Rational{BigInt}, k) # Tautology case
+            continue
+        else
+            scope_map = groupby(n -> sort(variables(n.vtree)), nodes)
+            filtered = filter((k, v) -> length(k) > var_thresh && length(v) > 1, scope_map)
+            top_candidates = Dict(k => sort(v, by=x->length(node2dag(x)), rev=true) for (k,v) in filtered)
+
+            if length(values(top_candidates)) == 0
+                continue
+            end
+
+            # For now pick top two and put them as candidates
+            push!(candidates, [ns[1:2] for ns in values(top_candidates)]...)
+        end
+    end
+
+    candidates
+end
 
 """
 Randomly picking egde and variable from candidates
