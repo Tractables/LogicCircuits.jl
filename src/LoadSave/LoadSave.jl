ddb57064e8b02598c26e9776bf5a755fcd37d8f9
Guy Van den Broeck <guyvdb@gmail.com>
2020-07-25 21:38:18 -0700

    remove line

--- src/LoadSave/LoadSave.jl
+++ src/LoadSave/LoadSave.jl
@@ -3,8 +3,6 @@ module LoadSave # can no longer be called `IO` because it conflicts with `Base.I
 using ..Utils
 using ...LogicCircuits
 
-const zoo_version = "/Circuit-Model-Zoo-0.1.2"
-
 include("vtree_line_compiler.jl")
 include("vtree_loaders.jl")
 include("vtree_savers.jl")
