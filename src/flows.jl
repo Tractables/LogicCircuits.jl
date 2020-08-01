daaf5935da67c1695e7e3d7639e9e7da736b1cce
Guy Van den Broeck <guyvdb@gmail.com>
2020-08-01 12:20:33 -0700

    allow for more flexible circuit evaluation + documentation on flows

--- src/flows.jl
+++ src/flows.jl
@@ -12,7 +12,10 @@ function (root::LogicCircuit)(data...)
     evaluate(root, data...)
 end
 
+"Input types supported by flow algorithms"
 const Batch = Union{<:AbstractMatrix, DataFrame}
+
+"A vector of some float type"
 const FloatVector = Vector{<:AbstractFloat}
 
 "Container for circuit flows represented as an implicit conjunction of a prime and sub bit vector (saves memory allocations in circuits with many binary conjunctions)"
@@ -21,13 +24,17 @@ struct UpFlow2{F}
     sub_flow::F
 end
 
+"Flows of type `F`, or their implicit representation as an `UpFlow2`"
 const UpFlow{F} = Union{F,UpFlow2{F}}
 
+"Implicit conjunction of two Boolean flows"
 const BitFlow2 = UpFlow2{BitVector}
+
+"Implicit conjunction of two Float flows"
 const FloatFlow2 = UpFlow2{<:FloatVector}
 
-"Container for circuit flows represented as a bit vector"
-flow_type(::BitArray) = BitVector
+"For a given input, what is the right type of the flow data?"
+flow_type(::AbstractMatrix{<:Bool}) = BitVector
 flow_type(m::AbstractMatrix{<:AbstractFloat}) = Vector{eltype(m)}
 flow_type(data::DataFrame) = begin
     isbinarydata(data) && return BitVector
@@ -46,7 +53,7 @@ function evaluate(root::LogicCircuit, data::Batch, ::Type{F};
     num_examples::Int = Utils.num_examples(data)
 
     @inline f_lit(n) = begin
-        fv = feature_values(data,variable(n))::F
+        fv::F = feature_values(data,variable(n))
         ispositive(n) ? fv : complement(fv)
     end
     
@@ -88,15 +95,15 @@ function evaluate(root::LogicCircuit, data::Batch, ::Type{F};
     return nsave(root, materialize(root_flow))::F
 end
 
+"Turn a implicit `UpFlow2{F}` into its concrete flow `F`"
 @inline materialize(elems) = elems
 @inline materialize(elems::UpFlow2) = flow_and(elems.prime_flow, elems.sub_flow)
 
+"Complement a flow (corresponding to logical negation)"
 @inline complement(x::BitVector) = broadcast(!,x)
 @inline complement(x::Vector{T}) where T<:AbstractFloat = @avx one(T) .- x
 
-# TODO: see if https://github.com/chriselrod/LoopVectorization.jl provides any speedups for our workload (espcially on Float flows)
-# TODO; create a version that doesn't allocate, using fold! and pre-allocated fields
-
+"Compute the logical and of two flow objects"
 @inline flow_and(x::BitVector, y::BitVector) = @. x & y
 @inline flow_and(x::FloatVector, y::FloatVector) = @avx @. x * y
 @inline flow_and(x::BitVector, y::BitFlow2) = @. x & y.prime_flow & y.sub_flow
@@ -105,6 +112,7 @@ end
 @inline flow_and(x::BitFlow2, y::BitFlow2) = @. x.prime_flow & x.sub_flow & y.prime_flow & y.sub_flow
 @inline flow_and(x::FloatFlow2, y::FloatFlow2) = @avx @. x.prime_flow * x.sub_flow * y.prime_flow * y.sub_flow
 
+"Compute the logical or of two flow objects"
 @inline flow_or(x::BitVector, y::BitVector) = @. x | y
 @inline flow_or(x::FloatVector, y::FloatVector) = @avx @. x + y
 @inline flow_or(x::BitVector, y::BitFlow2) = @. x | (y.prime_flow & y.sub_flow)
@@ -113,11 +121,13 @@ end
 @inline flow_or(x::BitFlow2, y::BitFlow2) = @. (x.prime_flow & x.sub_flow) | (y.prime_flow & y.sub_flow)
 @inline flow_or(x::FloatFlow2, y::FloatFlow2) = @avx @. (x.prime_flow * x.sub_flow) + (y.prime_flow * y.sub_flow)
 
+"And one logical flow with another in place"
 @inline flow_and!(x::BitVector, v::BitVector) = @. x = x & v; nothing
 @inline flow_and!(x::FloatVector, v::FloatVector) = @avx @. x = x * v; nothing
 @inline flow_and!(x::BitVector, v::BitFlow2) = @. x = x & (v.prime_flow & v.sub_flow); nothing
 @inline flow_and!(x::FloatVector, v::FloatFlow2) = @avx @. x = x * (v.prime_flow * v.sub_flow); nothing
 
+"Or one logical flow with another in place"
 @inline flow_or!(x::BitVector, v::BitVector) = @. x = x | v; nothing
 @inline flow_or!(x::FloatVector, v::FloatVector) = @avx @. x = x + v; nothing
 @inline flow_or!(x::BitVector, v::BitFlow2) = @. x = x | (v.prime_flow & v.sub_flow); nothing
@@ -127,6 +137,7 @@ end
 # downward pass
 #####################
 
+"Container for both the upward and downward flow of a given type `F`"
 struct UpDownFlow1{F}
     upflow::F
     downflow::F
@@ -134,10 +145,13 @@ struct UpDownFlow1{F}
         new{F}(upf, never(F, length(upf)))
 end
 
+"Container for the upward implicit flow of type `F`, for which no downward flow is stored"
 const UpDownFlow2{F} = UpFlow2{F}
 
+"Any container for up/down flow"
 const UpDownFlow{F} = Union{UpDownFlow1{F},UpDownFlow2{F}}
 
+"Compute the logical or probabilistic flow of `data` through this `circuit`"
 function compute_flows(circuit::LogicCircuit, data::Batch)
     compute_flows(circuit, data, flow_type(data))
 end
@@ -199,12 +213,13 @@ function compute_flows(circuit::LogicCircuit, data::Batch, ::Type{F}) where F
     nothing
 end
 
-
+"Accumulate the downflow from an And node to its children"
 @inline acc_downflow_and(downflow_c::BitVector, downflow_n::BitVector) =
     @. downflow_c |= downflow_n
 @inline acc_downflow_and(downflow_c::FloatVector, downflow_n::FloatVector) =
     @avx @. downflow_c += downflow_n
     
+"Accumulate the downflow from an Or node to its children"
 @inline acc_downflow_or(downflow_c::BitVector, downflow_n::BitVector, 
                         upflow1_c::BitVector, ::BitVector) =
     @. downflow_c |= downflow_n & upflow1_c
@@ -212,6 +227,7 @@ end
                                upflow1_c::FloatVector, upflow_n::FloatVector) =
     @avx @. downflow_c += downflow_n * upflow1_c / upflow_n
 
+# the following methods skip children with implicit flows, and push the flow down to the grandchildren immediately
 @inline acc_downflow_or(downflow_gc::BitVector, downflow_n::BitVector, 
                                upflow2_c::BitFlow2, ::BitVector) =
     @. downflow_gc |= downflow_n & upflow2_c.prime_flow & upflow2_c.sub_flow
--- src/queries.jl
+++ src/queries.jl
@@ -188,15 +188,12 @@ end
 #####################
 
 "Evaluate the circuit bottom-up for a given input"
-evaluate(root::LogicCircuit, data::Bool...) =
+evaluate(root::LogicCircuit, data::Real...) =
     evaluate(root, collect(data))
 
-function evaluate(root::LogicCircuit, data::AbstractVector{Bool})::Bool
-    f_con(n) = istrue(n)
-    f_lit(n) = ispositive(n) ? data[variable(n)] : !data[variable(n)]
-    f_a(n, call) = mapreduce(call, &, children(n))
-    f_o(n, call) = mapreduce(call, |, children(n))
-    foldup(root, f_con, f_lit, f_a, f_o, Bool)
+function evaluate(root::LogicCircuit, data::AbstractVector{<:Real})
+    batch_data = reshape(data, 1, length(data)) # create a fake batch of 1 data point
+    evaluate(root, batch_data)[1] # run the batch algorithm
 end
 
 """
