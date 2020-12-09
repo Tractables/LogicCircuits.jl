1cd3fda02fa7bd82d1fa02898ee404edce0d7b14
Pasha Khosravi <khosravipasha@gmail.com>
2020-12-08 18:10:01 -0800

    update artifact

--- src/LoadSave/circuit_loaders.jl
+++ src/LoadSave/circuit_loaders.jl
@@ -17,7 +17,7 @@ export zoo_cnf,
 # loaders from model zoo
 #####################
 
-const zoo_version = "/Circuit-Model-Zoo-0.1.2"
+const zoo_version = "/Circuit-Model-Zoo-0.1.3"
 
 zoo_cnf(name) = 
     load_cnf(zoo_cnf_file(name))
