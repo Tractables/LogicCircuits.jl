0b0b44b78ca64f3722b228343cf664d57b9bf2c0
Guy Van den Broeck <guyvdb@gmail.com>
2021-03-12 20:44:10 -0800

    pretty line breaks on small screens

--- docs/src/README.jl
+++ docs/src/README.jl
@@ -4,7 +4,7 @@
 
 # <!-- DO NOT EDIT README.md directly, instead edit docs/README.jl and generate the markdown-->
 
-# # LogicCircuits.jl
+# # Logic<wbr>Circuits<wbr>.jl
 
 # [![Unit Tests](https://github.com/Juice-jl/LogicCircuits.jl/workflows/Unit%20Tests/badge.svg)](https://github.com/Juice-jl/LogicCircuits.jl/actions?query=workflow%3A%22Unit+Tests%22+branch%3Amaster) [![codecov](https://codecov.io/gh/Juice-jl/LogicCircuits.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/Juice-jl/LogicCircuits.jl) [![](https://img.shields.io/badge/docs-stable-green.svg)](https://juice-jl.github.io/LogicCircuits.jl/stable) [![](https://img.shields.io/badge/docs-dev-blue.svg)](https://juice-jl.github.io/LogicCircuits.jl/dev)
 
